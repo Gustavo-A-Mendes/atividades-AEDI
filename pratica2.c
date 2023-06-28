ad5d6d1ba97071daeb3035e62f4c3aa8333f328b
Gustavo-A-Mendes <gust4v0.a.mendes@gmail.com>
2023-06-28 17:21:41 -0300

    Modicação na pratica2 e add o programa teste

--- pratica2.c
+++ pratica2.c
@@ -8,6 +8,12 @@ int main(void) {
   float *ponteiro;
   ponteiro = &var;
 
+  printf("Var = %f \nPonteiro = %p\n", var, ponteiro);
+
+  // Modificar o valor da variável var através do ponteiro
+  *ponteiro = 3.7;
+
   printf("Var = %f \nPonteiro = %p", var, ponteiro);
+
 
 }
---
+++ teste.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void incrementa(int *ponteiro) {
+  /*
+    int *ponteiro: ponteiro para variável do tipo int
+    Realiza o incremento do conteúdo do ponteiro
+  */
+
+  (*ponteiro)++;
+}
+
+int main(void) {
+  int var = 2;
+  printf("var = %d\n", var);
+
+  incrementa(&var);
+  printf("var = %d\n", var);
+
+  return 0;
+}
