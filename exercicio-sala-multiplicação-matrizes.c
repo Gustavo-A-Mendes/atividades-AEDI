21fcef1dee08ec7ee5453c8e5121162c2b269476
Gustavo-A-Mendes <gust4v0.a.mendes@gmail.com>
2023-07-04 21:22:21 -0300

    adicinando atividade de sala

---
+++ exercicio-sala-multiplicação-matrizes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alocaMatriz(int, int);
+
+int main(void) {
+  int linhas = 5, colunas = 5;
+
+  int **matrizA = (int **)alocaMatriz(linhas, colunas);
+
+  int linha, coluna;
+  printf("\n");
+  for (linha = 0; linha < linhas; linha++) {
+    for (coluna = 0; coluna < colunas; coluna++) {
+
+      matrizA[linha][coluna] = 5*linha + coluna;
+      printf("%d\t", matrizA[linha][coluna]);
+    }
+    printf("\n");
+  }
+
+  for (linha = 0; linha < linhas; linha++) {
+    free(matrizA[linha]);
+  }
+  free(matrizA);
+
+  return 0;
+}
+
+int **alocaMatriz(int Ls, int Cs) {
+  // Aloca espaço na memória para a matriz
+  int (**matriz) = (int **)malloc(Ls * sizeof(int *));
+  if (matriz == NULL) {
+    printf("Erro: Memória insuficiente.\n");
+    exit(1);
+  }
+  int l;
+  
+  for (l = 0; l < Ls; l++) {
+    matriz[l] = (int *)malloc(Cs * sizeof(int));
+    if (matriz[Ls] == NULL) {
+    printf("Erro: Memória insuficiente.\n");
+    exit(1);
+    }
+  }
+  return matriz;
+}
