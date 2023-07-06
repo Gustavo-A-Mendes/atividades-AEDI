1ecb2554f87650cfa73c76eb4212d10a07150c81
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-07-06 00:38:45 -0300

    Atualizando questões

--- pratica laboratorio/questao-matrizes/questao-03.c
+++ pratica laboratorio/questao-matrizes/questao-03.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linha, coluna;
 int **alocaMatrizQ(int);
--- pratica laboratorio/questao-matrizes/questao-04.c
+++ pratica laboratorio/questao-matrizes/questao-04.c
@@ -9,10 +9,14 @@ int main (void) {
   printf("Digite o numero de pessoas: ");
   scanf("%d", &n);
 
-
+  int i;
   // aloca memoria para o vetor referente ao nome
-  char *nome = (char*)calloc(n,sizeof(char));
+  char **nome = (char**)calloc(n,sizeof(char*));
   if (nome == NULL) exit(1); // Falha ao alocar memória
+  // for (i = 0; i < n; i++) {
+  //   nome[i] = (char*)calloc(n,sizeof(char));
+  // }
+
 
   // aloca memoria para o vetor referente a idade
   int *idade = (int*)calloc(n,sizeof(int));
@@ -20,14 +24,14 @@ int main (void) {
 
   int i;
   for (i = 0; i < n; i++) {
-    printf("Digite o nome completo do usuario: ");
+    printf("Digite o nome completo do usuario: \n");
     scanf("%s[^\n]", nome + i);
     printf("Digite a idade do usuario: ");
     scanf("%d", idade + i);
   }
 
   for (i = 0; i < n; i++) {
-    printf("\n\n%c tem %d anos de idade", *(nome+i), *(idade+i));
+    printf("\n\n%s tem %d anos de idade", *(nome+i), *(idade+i));
   }
   
 
