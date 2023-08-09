a2e1e37159b050383642cd137fc12230fd86f04f
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-09 09:05:56 -0300

    upload

--- exercicios de fixacao/unidade-2/7_arquivo/lista02/questao03/questao03.c
+++ exercicios de fixacao/unidade-2/7_arquivo/lista02/questao03/questao03.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 
 
-#define MAX_FRUTA 100
+#define MAX_FRUTA 5
 
 typedef struct frutas {
   char nome[21];
@@ -12,9 +12,9 @@ typedef struct frutas {
 } Frutas;
 
 
-void copia_dados(FILE*, Frutas**);
+int copia_dados(FILE*, Frutas**);
 
-int i;
+int i, count;
 
 int main(void) {
 
@@ -28,7 +28,10 @@ int main(void) {
     printf("\nArquivo aberto com sucesso!\n");
   }
 
-  copia_dados(saida, var_frutas);
+  count = copia_dados(saida, var_frutas);
+  
+  for (i = 0; i < count; i++) free(var_frutas[i]);
+  free(var_frutas);
 
   fclose(saida);
 
@@ -44,7 +47,7 @@ int main(void) {
     printf("%c", s);
   }
   
-  printf("\nTodas as frutas foram cadastradas.\nObrigado por usar nosso programa\n");
+  printf("\nTodas as frutas foram cadastradas.\nObrigado por usar nosso programa.\n");
   
   fclose(saida);
   
@@ -53,30 +56,40 @@ int main(void) {
 }
 
 
-void copia_dados(FILE *fl, Frutas** frutas) {
+int copia_dados(FILE *fl, Frutas** frutas) {
   
   fprintf(fl, "%s,%s\n", "NOME", "PRECO");
 
   while (1) {
-    int count_fruta = 0;
-    char resp[2];
+    if (i < MAX_FRUTA) {
+      int count_fruta = 0;
+      char resp[2];
 
-    frutas[i] = (Frutas*)malloc(sizeof(Frutas));
+      frutas[i] = (Frutas*)malloc(sizeof(Frutas));
 
-    printf("\n===== Fruta %d =====\n", i+1);
-    printf("Insira o nome: ");
-    scanf(" %20[^\n]", frutas[i]->nome);
-    printf("Insira o preco: ");
-    scanf("%f", &frutas[i]->preco);
+      printf("\n===== Fruta %d =====\n", i+1);
+      printf("Insira o nome: ");
+      scanf(" %20[^\n]", frutas[i]->nome);
+      printf("Insira o preco: ");
+      scanf("%f", &frutas[i]->preco);
 
-    fprintf(fl, "%s,%-.2f\n", frutas[i]->nome, frutas[i]->preco);
-
-    printf("\nDeseja continuar? [S / N]\n");
-    scanf(" %1[^\n]", resp);
-    resp[0] = toupper(resp[0]);
-
-    if (resp[0] == 'S') {
+      fprintf(fl, "%s,%-.2f\n", frutas[i]->nome, frutas[i]->preco);
       i++;
-    } else break;
+      // printf("%d\n", i);
+      if (i < MAX_FRUTA) {
+        printf("\nDeseja continuar? [S / N]\n");
+        scanf(" %1[^\n]", resp);
+        resp[0] = toupper(resp[0]);
+        if (resp[0] == 'N') break;
+      } else {
+        printf("\nNumero maximo de Frutas cadastradas\n");
+        break;
+      }
+    } else {
+      printf("\nErro! Não há mais espaço para cadastro de frutas.\n");
+      break;
+    }
   }
+
+  return i;
 }
